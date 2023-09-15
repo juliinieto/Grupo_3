c6b5e2871abd55e33ec1ec18b8f39e6c26e35d76
Caallende <132697803+Caallende@users.noreply.github.com>
2023-09-15 14:40:15 -0300

    Add files via upload
    inicio

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Página de Inicio</title>
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1>Bienvenido al Sistema de Emergencias Médicas</h1>
-    <p>Por favor, inicie sesión o regístrese para continuar.</p>
-    <a href="login.php">Iniciar Sesión</a>
-    <a href="registro.php">Registrarse</a>
+    <div class="container">
+        <h1>Bienvenido a la página principal</h1>
+        <div class="buttons">
+            <a href="login.php?role=1" class="button">Alumno</a>
+            <a href="login.php?role=2" class="button">Profesor</a>
+        </div>
+    </div>
 </body>
 </html>
