b4ff5ad7524662d8e464289a5334329347e483be
Caallende <132697803+Caallende@users.noreply.github.com>
2023-09-28 14:37:17 -0300

    Add files via upload

---
+++ hospital/doctor/doctorP.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doctores</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<?php include 'headerP.php'; ?>
+
+    <h1>Doctores</h1>
+    
+    <div class="doctor2">
+        <img src="Ellen.jpeg" alt="Dr. Juan Pérez">
+        <h2>Dr. Juan Pérez</h2>
+        <p>Especialidad: Cardiología</p>
+    </div>
+
+    <div class="doctor1">
+        <img src="doc.jpeg" alt="Dra. María Rodríguez">
+        <h2>Dra. María Rodríguez</h2>
+        <p>Especialidad: Dermatología</p>
+    </div>
+
+    <div class="doctor">
+        <img src="patrik.jpeg" alt="Dr. Carlos González">
+        <h2>Dr. Carlos González</h2>
+        <p>Especialidad: Pediatría</p>
+    </div>
+    <div class="doctor">
+        <img src="sandra.jpeg" alt="Dr. Carlos González">
+        <h2>Dr. Carlos González</h2>
+        <p>Especialidad: Pediatría</p>
+    </div>
+    <div class="doctor">
+        <img src="patrik.jpeg" alt="Dr. Carlos González">
+        <h2>Dr. Carlos González</h2>
+        <p>Especialidad: Pediatría</p>
+    </div>    <div class="doctor">
+        <img src="doctora1.jpeg" alt="Dr. Carlos González">
+        <h2>Dr. Carlos González</h2>
+        <p>Especialidad: Pediatría</p>
+    </div>
+
+</body>
+</html>
